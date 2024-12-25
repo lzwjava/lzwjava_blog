66eef81f788bbb693cfcd52b65266c4d63c70375
Zhiwei Li <lzwjava@gmail.com>
2024-12-25 10:27:30 +0800

    add

--- assets/_posts/2021-08-05-showmebug-zh.md
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title: "在创业公司的一次演讲 - 我享受的事情"
----
-
-我在 2021 年 8 月的 ShowMeBug 上做了一次演讲。主题是一些我享受的事情。你可以在 [GitHub](https://github.com/lzwjava/Keynotes/blob/master/ShowMeBug/20210805lzw.pdf) 上找到演示文稿。
-
-我分享了有关一个电子项目、Kindle 技巧以及隐王的博客，还有我的英语学习。
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'blog_list_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Blog',
+      title: 'Hello World',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BlogListPage(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
     );
   }
 }
